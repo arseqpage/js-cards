f44fae0fa950b6cd86bb48d089cb35252ec932ee
Arslan <arseqpage@gmail.com>
2021-07-24 14:01:13 +0300

    Api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Card app2');
+import 'regenerator-runtime/runtime';
+
+import { API_URL, URL_COMICS,  } from './constants/api';
+import {getDataApi} from './utils/getDataApi';
+
+
+(async () => {
+  const data = await getDataApi.getData(API_URL + URL_COMICS);
+
+  if (data) {
+    //
+  } else {
+    //
+  }
+
+  console.log(data);
+})();
