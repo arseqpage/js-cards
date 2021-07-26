dd889178badc5c2c266a8f6d13c1a8c2fb0be23d
Arslan <arseqpage@gmail.com>
2021-07-26 09:36:21 +0300

    Scrollbar

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import { API_URL, URL_COMICS } from '../../constants/api';
+import { getDataApi } from '../../utils/getDataApi';
+
+import './App.css';
+
+class App {
+  async render() {
+    const data = await getDataApi.getData(API_URL + URL_COMICS);
+
+    if (data) {
+      //
+    } else {
+      //
+    }
+
+    console.log(data);
+  }
+}
+
+export default new App();
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import 'regenerator-runtime/runtime';
 
-import { API_URL, URL_COMICS,  } from './constants/api';
-import {getDataApi} from './utils/getDataApi';
-
+import App from './components/App';
 
 (async () => {
-  const data = await getDataApi.getData(API_URL + URL_COMICS);
-
-  if (data) {
-    //
-  } else {
-    //
-  }
-
-  console.log(data);
+  await App.render();
 })();
